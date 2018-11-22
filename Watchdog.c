502301e01c9eeb62099a6a67c17c288b408be8d3
PierreLanderouin <36203441+PierreLanderouin@users.noreply.github.com>
2018-11-22 11:29:25 +0100

    Add files via upload

---
+++ Watchdog.c
@@ -0,0 +1,25 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <Count.h>
+#include <Watchdog.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void *watchDog(void)
+{
+	int tampon = 0;
+	while()
+	{
+		pthread_mutex_lock(&mutex);
+		if(GetCount()<=tampon)
+		{
+			printf("Oh probleme");
+			//Lancer le thread Backup
+		}
+	tampon = GetCount();
+	pthread_mutex_unlock(&mutex);
+	}
+}
+
+
+
