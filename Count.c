de314b410af3f153f1849d54d19f4f8e35851a7c
TaraLemaire <mat0lemaire@gmail.com>
2018-11-22 11:50:22 +0100

    count function test

---
+++ Count.c
@@ -0,0 +1,16 @@
+#include "Count.h"
+
+static int Count = 0;
+
+inline int IncCount(void){
+	Count+=1;
+	return(Count);
+}
+
+inline int GetCount(void){
+	return(Count);
+}
+
+inline void ClearCount(void){
+	Count = 0;
+}
