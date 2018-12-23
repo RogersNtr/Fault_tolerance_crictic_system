304d88ac73a981ecd0e2c089081c88a4344eb411
RogersNtr <r.nengwe@yahoo.fr>
2018-12-23 18:37:41 +0100

    data generation

---
+++ generation_valeur.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+
+void sensor_value(int nber_range){    
+    /**
+     * nber_length : the number of number to generate
+     * nber_range: the function generates  numbers between[0, nber_range]
+     * return : print the resulting generate value in a file.
+    */
+   FILE* data_file=fopen("sensor_data.txt", "w+");
+   int rand_nber=0;
+   for(int i=0; i<=nber_range;i++){
+       rand_nber = rand()%nber_range + 1;
+       fprintf(data_file, "%d\n", rand_nber);
+   }
+}
+
+//Unit Test
+int main(){
+    int nber_range = 10000;
+    sensor_value(nber_range);
+}
